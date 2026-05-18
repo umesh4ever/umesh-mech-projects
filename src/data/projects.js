7efd1cbebad9d0d80e0d8c59427b934ff8ba1bd9
mesh <154255289+umesh4ever@users.noreply.github.com>
2026-05-18 18:44:02 +0530

    feat: Refactor project details layout and enhance content presentation in ProjectDetail component

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,86 +1,71 @@
 const projects = {
   cmm: {
-    type : "cad",
+    type: "cad",
     title: "Gantry CMM Model",
-    objective: "The objective is to design a model of Gantry CMM the biggest type of CMMs.",
-    purpose:"To get comfortable with complex modeling with multiple parts and set motion restrictions.",
-    tool:"I have used fusion 360.",
+    objective:
+      "The objective is to design a model of Gantry CMM the biggest type of CMMs.",
+    purpose:
+      "To get comfortable with complex modeling with multiple parts and set motion restrictions.",
+    tool: "I have used fusion 360.",
     model: "models/cmm.glb",
     modelCaption: "Gantry CMM CAD model",
     cover: "coverImages/cmmCover.png",
     videos: ["videos/GantryCMMParts.mp4", "videos/GantryCMMWorking.mp4"],
-    captions: ["Gantry CMM model destructured","Gantry working mechanism"],
-   
+    videoCaptions: [
+      "Gantry CMM model destructured",
+      "Gantry working mechanism",
+    ],
+    whatItIs: [
+      "CMM stands for a Coordinate Measuring Machine, which is a highly precise device used in manufacturing to measure the exact physical dimensions of an object by touching it with a probe or scanning it with a laser to ensure it matches engineering designs. Depending on your context.",
+
+      "A gantry is an overhead, bridge-like framework designed to span across an open area to support, lift, or move heavy objects. It consists of two parallel side rails (or legs) connected by a horizontal crossbeam that travels along them, commonly seen in massive shipping port cranes, highway sign structures, and heavy manufacturing.",
+
+      "When applied to a Coordinate Measuring Machine (CMM), this gantry structure creates a completely open workspace on the factory floor, allowing a precise measuring probe to travel smoothly across all three geometric axes to inspect enormous parts.",
+
+      "A gantry CMM functions through six essential parts working together: a flush concrete or granite FOUNDATION built into the floor to support massive parts, and two elevated, parallel GUIDEWAYS that guide the machine's forward and backward movement. A heavy horizontal GANTRY BEAM spans across these tracks to move side-to-side, while a vertical CARRIAGE AND RAM extends downward from the beam to reach into deep components. At the very tip of this ram is the MEASURING PROBE (a touch sensor or laser scanner) that captures physical points, all regulated by a computerized CONTROLLER that translates the movement into precise 3D data.",
+    ],
   },
 
-
-
-
-
-
-
-
-
-
-
-
   geneva: {
-    type : "cad",
+    type: "cad",
     title: "Geneva Mechanism",
-    objective: "The objective is to create a Ganeva mechanism model and to demonstrate through a motion video.",
-    purpose : "To learn fusion 360 cad modelling and motion study with relative contact",
-    tool:"I have used fusion 360.",
+    objective:
+      "The objective is to create a Ganeva mechanism model and to demonstrate through a motion video.",
+    purpose:
+      "To learn fusion 360 cad modelling and motion study with relative contact",
+    tool: "I have used fusion 360.",
     model: "models/geneva.glb",
     modelCaption: "Geneva CAD model",
     cover: "coverImages/genevaCover.png",
     videos: ["videos/GenevaParts.mp4", "videos/GenevaWorking.mp4"],
-    captions: ["Geneva model destructured","Geneva working mechanism"],
-
-
-    
+    videoCaptions: ["Geneva model destructured", "Geneva working mechanism"],
   },
 
-
-
-
-
-
-
-
-
-
-
   drs: {
-    type : "simulation",
+    type: "simulation",
     title: "Drag Reduction System (DRS)",
     objective: "Not defined yet",
     model: "models/turbine.glb",
     cover: "coverImages/placeholder.png",
     videos: ["videos/turbine-parts.mp4", "videos/turbine-working.mp4"],
-    
   },
 
-
-
-
-
-
-
-
-
-
-
-
-
-
   truck: {
-    type : "simulation",
+    type: "simulation",
     title: "CFD Analysis of Truck Aerodynamics",
-    objective: "objective is to perform a CFX analysis and measure the aerodynamics lift and drag forces.",
+    objective:
+      "objective is to perform a CFX analysis and measure the aerodynamics lift and drag forces.",
     model: "",
     cover: "coverImages/truckCover.png",
-    images: ["videos/GenevaParts.mp4", "videos/GenevaWorking.mp4"],
-   
+    preResultImages :[],
+    resultImages: [
+      "truck/dragForce.png",
+      "truck/liftForce.png",
+      "truck/pressureContour.png",
+      "truck/streamline.png",
+      "truck/velocityContour.png",
+    ],
+    resultImageCaptions: ["Drag Force","Lift Force","Pressure Contour","Streamline","Velocity Contour"],
   },
 };
 
--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -15,69 +15,123 @@ const ProjectDetail = () => {
 
   return (
     <section className="w-full py-16 px-6 ">
-      <div className="max-w-6xl mx-auto space-y-10 lg:px-30">
-        <h1 className="text-6xl text-center font-bold font-serif">
+      <div className="max-w-6xl bg-white mx-auto space-y-10 lg:px-30 lg:border-l-2 lg:border-r-2 border-blue-800">
+        <h1 className=" text-4xl lg:text-6xl text-center font-bold font-serif">
           {project.title}
         </h1>
 
         <div id="introInfo">
-          <h2 className="text-2xl my-2 ">
+          <h2 className="text-xl lg:text-2xl my-2 ">
             <span className="font-bold text-blue-800">Objective : </span>
             {project.objective}
           </h2>
-          <h2 className="text-2xl my-2 ">
+          <h2 className="text-xl lg:text-2xl my-2 ">
             <span className="font-bold text-blue-800">Why : </span>
             {project.purpose}
           </h2>
-          <h2 className="text-2xl my-2 ">
+          <h2 className="text-xl lg:text-2xl my-2 ">
             <span className="font-bold text-blue-800">Tools used : </span>
             {project.tool}
           </h2>
         </div>
 
         {/* 3D Model */}
-        <div id="modelDiv">
-          <h1 className="text-4xl text-center my-4 font-bold ">
-            Model Demonstration
-          </h1>
-          <model-viewer
-            src={`${import.meta.env.BASE_URL}${project.model}`}
-            camera-controls
-            auto-rotate
-            className="w-auto lg:mx-0 h-96 rounded-xl bg-linear-to-br from-slate-800 via-slate-700 to-slate-900 shadow-xl"
-          ></model-viewer>
-          <p className="text-center mt-2 text-2xl font-semibold text-blue-800">
-            {project.modelCaption}
-          </p>
+        {project.model && (
+          <div id="modelDiv">
+            <h1 className="text-4xl text-center my-4 font-bold">
+              Model Demonstration
+            </h1>
+
+            <model-viewer
+              src={`${import.meta.env.BASE_URL}${project.model}`}
+              camera-controls
+              auto-rotate
+              className="w-auto lg:mx-0 h-96 rounded-xl bg-linear-to-br from-slate-800 via-slate-700 to-slate-900 shadow-xl"
+            ></model-viewer>
+
+            <p className="text-center mt-2 text-2xl font-semibold text-blue-800">
+              {project.modelCaption}
+            </p>
+          </div>
+        )}
+
+        {/* theory */}
+
+        <div>
+          <h2 className="text-2xl my-2">
+            <span className="font-bold text-blue-800">Theory :</span>
+          </h2>
+
+          {project.whatItIs?.map((para, index) => (
+            <p key={index} className="mb-4 text-xl lg:text-2xl leading-relaxed">
+              {para}
+            </p>
+          ))}
         </div>
 
         {/* Videos */}
-        <div id="VideoDiv">
-          <h1 className="text-4xl text-center my-4 font-bold ">
-            Video Demonstration
-          </h1>
-          <div className="grid md:grid-cols-2 gap-8">
-            {project.videos?.filter(Boolean).map((video, index) => (
-              <div key={index}>
-                <video controls className="w-full rounded-lg">
-                  <source
-                    src={`${import.meta.env.BASE_URL}${video}`}
-                    type="video/mp4"
-                  />
-                  Your browser does not support the video tag.
-                </video>
+        {project.videos?.filter(Boolean).length > 0 && (
+          <div id="VideoDiv">
+            <h1 className="text-4xl text-center my-4 font-bold">
+              Video Demonstration
+            </h1>
 
-                <p className="text-center mt-2 text-2xl font-semibold text-blue-800">
-                  {project.captions?.[index]}
-                </p>
-              </div>
-            ))}
+            <div className="grid md:grid-cols-2 gap-8">
+              {project.videos.filter(Boolean).map((video, index) => (
+                <div key={index}>
+                  <video controls className="w-full rounded-lg">
+                    <source
+                      src={`${import.meta.env.BASE_URL}${video}`}
+                      type="video/mp4"
+                    />
+                    Your browser does not support the video tag.
+                  </video>
+
+                  <p className="text-center mt-2 text-2xl font-semibold text-blue-800">
+                    {project.videoCaptions?.[index]}
+                  </p>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        )}
 
         {/* Description */}
         <div className="space-y-6">
-          <h1 className="text-4xl text-center my-4  overline" >More Project Details will be updates in coming time.</h1>
+          <h1 className="text-2xl lg:text-4xl text-center my-4 text-red-600">
+            More Project Details will be added  soon.
+          </h1>
+        </div>
+
+        {/* Images */}
+        {project.resultImages?.filter(Boolean).length > 0 && (
+          <div id="ImageDiv" className="mt-16">
+            <h1 className="text-4xl text-center my-4 font-bold">
+              Image Results
+            </h1>
+
+            <div className="grid md:grid-cols-2 gap-8">
+              {project.resultImages.filter(Boolean).map((image, index) => (
+                <div key={index}>
+                  <img
+                    src={`${import.meta.env.BASE_URL}${image}`}
+                    alt={project.resultImageCaptions?.[index]}
+                    className="w-full rounded-lg shadow-lg object-cover"
+                  />
+
+                  <p className="text-center mt-2 text-xl font-semibold text-blue-800">
+                    {project.resultImageCaptions?.[index]}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+
+        <div className="space-y-6">
+          <h1 className="text-2xl lg:text-4xl text-center my-4 text-red-600">
+            Conclusion here.
+          </h1>
         </div>
       </div>
     </section>
